89a5358e6cd1cdbccc299cbdd245e8ea6d8372cc
DilmurodKh <dilmurod.py@gmail.com>
2025-03-25 14:37:08 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Main from "./components/Main";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO app</h1>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+
+
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
